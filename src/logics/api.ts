98f233e8bb4934a5476bfefe0669bc95bc50909b
ChoQ94 <whrbdnjs1994@gmail.com>
2023-08-12 13:14:59 +0900

    feat: mock 데이터 위치 변경

---
+++ src/logics/api.ts
@@ -0,0 +1,19 @@
+export const getRomanceList = async (page: string | number) => {
+  const getUrl = `http://localhost:3000/api/comics/romance/page_${page}.json`;
+  try {
+    const response = await fetch(getUrl).then((res) => res.json());
+    return response;
+  } catch (err) {
+    return {};
+  }
+};
+
+export const getDramaList = async (page: string | number) => {
+  const getUrl = `http://localhost:3000/api/comics/romance/page_${page}.json`;
+  try {
+    const response = await fetch(getUrl).then((res) => res.json());
+    return response;
+  } catch (err) {
+    return {};
+  }
+};
--- src/screen/RankPage.tsx
+++ src/screen/RankPage.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { getRomanceList } from "../logics/api";
+import ToonContainer from "../components/ToonContainer";
 
 export default function RankPage() {
   const navigate = useNavigate();
@@ -7,14 +9,27 @@ export default function RankPage() {
   const queryParams = new URLSearchParams(location.search);
   const genre = queryParams.get("genre");
 
+  const [data, setData] = useState();
+
+  const getData = async (page: number | string) => {
+    const res = await getRomanceList(page);
+    setData(res);
+  };
+
+  console.log(data);
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
     const genre = queryParams.get("genre");
-
+    getData(1);
     if (!genre) {
       navigate("/ranking?genre=romance");
     }
   }, [location.search, navigate]);
 
-  return <>{genre}</>;
+  return (
+    <>
+      {genre}
+      <ToonContainer />
+    </>
+  );
 }
