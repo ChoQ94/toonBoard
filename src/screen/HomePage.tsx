f55c9d18ffc98aa553fd059c2d931e7a77cbae39
ChoQ94 <whrbdnjs1994@gmail.com>
2023-08-13 11:12:43 +0900

    feat: 홈화면 수정

--- src/screen/HomePage.tsx
+++ src/screen/HomePage.tsx
@@ -8,8 +8,23 @@ export default function HomePage() {
   };
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ color: "white", marginTop: "200px" }}>
+        조규원(프런트 과제)
+      </div>
       <button
+        style={{
+          width: "200px",
+          height: "50px",
+          margin: "20px",
+          cursor: "pointer",
+        }}
         onClick={() => {
           routeToRanking("romance");
         }}
@@ -17,12 +32,21 @@ export default function HomePage() {
         로맨스 장르 랭킹
       </button>
       <button
+        style={{
+          width: "200px",
+          height: "50px",
+          margin: "20px",
+          cursor: "pointer",
+        }}
         onClick={() => {
           routeToRanking("drama");
         }}
       >
         드라마 장르 랭킹
       </button>
+      <div style={{ color: "white", marginTop: "200px" }}>
+        * 랭킹 페이지에서 로고버튼을 누르면 다시 이 화면으로 돌아옵니다.
+      </div>
     </div>
   );
 }
